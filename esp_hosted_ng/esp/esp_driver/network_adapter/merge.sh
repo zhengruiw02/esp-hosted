50c52fbb21203777db9a5301cb8bf4627372f8ba
Henry Zheng <zhengruiw02@gmail.com>
2026-01-05 14:31:38 +0800

    Add merge binary script

---
+++ esp_hosted_ng/esp/esp_driver/network_adapter/merge.sh
@@ -0,0 +1,1 @@
+python -m esptool --chip esp32c6 merge_bin --output esp_hosted_ng_esp32c6_merged.bin  0x0 build/bootloader/bootloader.bin 0x8000 build/partition_table/partition-table.bin 0xd000 build/ota_data_initial.bin 0x10000 build/network_adapter.bin
